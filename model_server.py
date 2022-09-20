23df72b5959abd8c2235bbdb4150d435e02a2b8f
Sami Liedes <sami.liedes@iki.fi>
2022-09-20 20:08:14 +0200

    Model server: working code for txt2img

--- model_server.py
+++ model_server.py
@@ -4,6 +4,8 @@ import asyncio
 from dataclasses import dataclass, field
 import threading
 
+import numpy as np
+
 # from concurrent.futures import ThreadPoolExecutor
 from typing import Tuple, Optional, Any
 from io import BytesIO
@@ -30,9 +32,6 @@ from google.protobuf.timestamp_pb2 import Timestamp
 
 import torch
 from torch import autocast
-from PIL import Image
-
-DEFAULT_PRIORITY = 100
 
 # disable safety checker if wanted
 def _dummy_checker(images, **kwargs):
@@ -45,7 +44,13 @@ class RequestQueueItem:
     request: ImGenRequest = field(compare=False)
     response: ImGenResponse = field(compare=False)  # some fields prepopulated when inserted
     future: Optional[asyncio.Future[ImGenResponse]] = field(compare=False)
-    priority: int = field(default=DEFAULT_PRIORITY)
+    priority: int
+
+
+def timestamp_now() -> Timestamp:
+    t = Timestamp()
+    t.GetCurrentTime()
+    return t
 
 
 # TODO: more graceful handling of KeyboardInterrupt / a stop() method etc.
@@ -61,7 +66,6 @@ class GPUWorker:
     _start_worker_task: asyncio.Task
 
     def __init__(self):
-        self._gpu_lock = asyncio.Lock()
         self._a_request_queue: asyncio.PriorityQueue[RequestQueueItem] = asyncio.PriorityQueue()
         self._eventloop = asyncio.get_event_loop()
         self._start_worker_task = asyncio.create_task(self._start_worker(), name="_start_worker")
@@ -69,9 +73,8 @@ class GPUWorker:
     @logger.catch
     async def _start_worker(self) -> None:
         logger.info("Loading text2img pipeline")
-        StableDiffusionPipeline.from_pretrained = logger.catch(StableDiffusionPipeline.from_pretrained)
         a_pipe_fut = asyncio.to_thread(
-            StableDiffusionPipeline.from_pretrained,
+            logger.catch(StableDiffusionPipeline.from_pretrained),
             pretrained_model_name_or_path=env.MODEL_DATA,
             revision=env.MODEL_REVISION,
             torch_dtype=env.TORCH_DTYPE,
@@ -80,7 +83,7 @@ class GPUWorker:
 
         logger.info("Loading img2img pipeline")
         a_img2imgpipe_fut = asyncio.to_thread(
-            StableDiffusionImg2ImgPipeline.from_pretrained,
+            logger.catch(StableDiffusionImg2ImgPipeline.from_pretrained),
             pretrained_model_name_or_path=env.MODEL_DATA,
             revision=env.MODEL_REVISION,
             torch_dtype=env.TORCH_DTYPE,
@@ -97,12 +100,39 @@ class GPUWorker:
             self._a_img2imgpipe.safety_checker = _dummy_checker
 
         self._thread = threading.Thread(target=self._w_worker_main, name="GPUWorker")
+        self._thread.start()
 
     @logger.catch
     def _w_worker_main(self) -> None:
+        logger.info("_w_worker_main started")
         while True:
-            rqitem = asyncio.run_coroutine_threadsafe(self._a_request_queue.get(), self._eventloop).result()
-            logger.debug("Worker received request with priority {}", rqitem.priority)
+            try:
+                rqitem = asyncio.run_coroutine_threadsafe(self._a_request_queue.get(), self._eventloop).result()
+                logger.debug("Worker received request with priority {}", rqitem.priority)
+                req = rqitem.request
+                meta = req.req_metadata
+                resp = rqitem.response
+                resp.resp_metadata.start_processing_time.GetCurrentTime()
+                if meta.test_no_compute:
+                    im = np.ndarray((0, 0), dtype=np.float32)
+                    prompt = ""
+                else:
+                    im, prompt = self._w_generate_image(
+                        prompt=meta.prompt,
+                        seed=meta.seed,
+                        height=meta.height,
+                        width=meta.width,
+                        num_inference_steps=meta.iterations,
+                        strength=meta.strength,
+                        guidance_scale=meta.guidance_scale,
+                        photo=req.image,
+                    )
+                resp.resp_metadata.finish_processing_time.GetCurrentTime()
+                resp.image = im.tobytes()
+                # TODO: prompt_tokens
+                self._eventloop.call_soon_threadsafe(rqitem.future.set_result, resp)
+            except Exception as e:
+                self._eventloop.call_soon_threadsafe(rqitem.future.set_exception, e)
 
     @logger.catch
     def _w_generate_image(
@@ -114,25 +144,30 @@ class GPUWorker:
         num_inference_steps: int,
         strength: float,
         guidance_scale: float,
-        photo: Optional[bytes] = None,
-    ) -> Tuple[Image.Image, str]:
-        logger.info("generate_image (photo={}): {}", photo is not None, prompt)
+        photo: bytes = b"",
+    ) -> Tuple[np.ndarray, str]:
+        logger.info("generate_image (photo={}): {}", bool(photo), prompt)
         generator = torch.cuda.manual_seed_all(seed)
 
-        if photo is not None:
+        # TODO: Make img2img work
+        photo = b""
+
+        if photo:
             self._a_pipe.to("cpu")
             self._a_img2imgpipe.to("cuda")
+            # TODO: float buffer
             init_image = Image.open(BytesIO(photo)).convert("RGB")
             init_image = init_image.resize((height, width))
             init_image = preprocess(init_image)
             with autocast("cuda"):
-                self.image = self._a_img2imgpipe(
+                image = self._a_img2imgpipe(
                     prompt=[prompt],
                     init_image=init_image,
                     generator=generator,
                     strength=strength,
                     guidance_scale=guidance_scale,
                     num_inference_steps=num_inference_steps,
+                    output_type="numpy",
                 )["sample"][0]
         else:
             self._a_pipe.to("cuda")
@@ -146,42 +181,30 @@ class GPUWorker:
                     width=width,
                     guidance_scale=guidance_scale,
                     num_inference_steps=num_inference_steps,
+                    output_type="numpy",
                 )["sample"][0]
-        return image
-
-    # Call from main thread
-    @logger.catch
-    async def generate_image_now(self, request: ImGenRequest) -> ImGenResponse:
-        async with self._gpu_lock():
-            image = await asyncio.to_thread(
-                self._generate_image,
-                kwargs=dict(
-                    prompt=request.prompt,
-                    seed=request.seed,
-                    height=request.height,
-                    width=request.width,
-                    num_inference_steps=request.iterations,
-                    strength=request.strength,
-                    guidance_scale=request.guidance_scale,
-                    photo=request.image,
-                ),
-            )
+        return image, ""
 
     @logger.catch
     async def _generate_image_via_queue(self, request: ImGenRequest) -> ImGenResponse:
-        response = ImGenResponse()
-        response.req_metadata = request.req_metadata
-        response.resp_metadata = ImGenResponseMetadata()
-        response.resp_metadata.request_received_time = Timestamp.GetCurrentTime()
-        item = RequestQueueItem(request=request, response=response, priority=request.priority)
-        item.resp_metadata.queue_size = self._a_request_queue.qsize()
-        item.future = self._eventloop.create_future()
+        response = ImGenResponse(
+            req_metadata=request.req_metadata,
+            resp_metadata=ImGenResponseMetadata(
+                request_received_time=timestamp_now(), queue_size=self._a_request_queue.qsize()
+            ),
+        )
+        item = RequestQueueItem(
+            request=request,
+            response=response,
+            priority=request.req_metadata.priority,
+            future=self._eventloop.create_future(),
+        )
         self._a_request_queue.put_nowait(item)
         return await item.future
 
     @logger.catch
     async def generate_image(self, request: ImGenRequest) -> ImGenResponse:
-        logger.debug("generate_image request")
+        logger.debug("generate_image (img2img={}): {}", bool(request.image), request.req_metadata.prompt)
         return await self._generate_image_via_queue(request)
 
     @logger.catch
@@ -197,7 +220,7 @@ class ModelServicer(model_server_pb2_grpc.ImGenServiceServicer):
 
     @logger.catch
     async def generate_image(self, request: ImGenRequest, context: grpc.aio.ServicerContext) -> ImGenResponse:
-        return ImGenResponse(prompt="test", iterations=100)
+        return await self.worker.generate_image(request)
 
     @logger.catch
     async def tokenize_prompt(self, request: TokenizeRequest, context: grpc.aio.ServicerContext) -> TokenizeResponse:
--- test_model_server.py
+++ test_model_server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from typing import Any, Dict
+
 import pytest
 import grpc
 
@@ -14,9 +16,35 @@ from model_server_pb2 import (
 from model_server_pb2_grpc import ImGenServiceStub
 
 import config
+import env
 
-channel = grpc.insecure_channel(f"localhost:{config.SERVER_PORT}")
-imgen = ImGenServiceStub(channel)
+_ImGenRequestMetadata_template = ImGenRequestMetadata(
+    width=env.WIDTH,
+    height=env.HEIGHT,
+    seed=1234,
+    iterations=env.NUM_INFERENCE_STEPS,
+    strength=env.STRENGTH,
+    guidance_scale=env.GUIDANCE_SCALE,
+)
 
-for i in range(10):
+
+def make_ImGenRequest(**kwargs: Dict[str, Any]) -> ImGenRequest:
+    meta = ImGenRequestMetadata()
+    meta.CopyFrom(_ImGenRequestMetadata_template)
+    for k, v in kwargs.items():
+        setattr(meta, k, v)
+    return ImGenRequest(req_metadata=meta)
+
+
+def main():
+    channel = grpc.insecure_channel(f"localhost:{config.SERVER_PORT}")
+    imgen = ImGenServiceStub(channel)
+
     print(imgen.tokenize_prompt(TokenizeRequest(prompt="Hello, world!")).prompt_tokens)
+    resp = imgen.generate_image(make_ImGenRequest(prompt="A ball on a floor", test_no_compute=False, iterations=2))
+    resp.image = f"{len(resp.image)} bytes of data".encode("utf-8")
+    print(resp)
+
+
+if __name__ == "__main__":
+    main()
